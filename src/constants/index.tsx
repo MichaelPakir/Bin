ae85f272b1743acaa5a3f381416b25d69d78c500
MichaelPakir <neovimm@gmail.com>
2024-11-08 15:57:46 +0800

    Add Sanity

---
+++ src/constants/index.tsx
@@ -0,0 +1,72 @@
+export const navBarList = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Shop",
+    link: "/shop",
+  },
+  {
+    title: "Cart",
+    link: "/cart",
+  },
+];
+
+export const footerData = [
+  {
+    _id: 2221,
+    title: "Get to know us",
+    listItem: [
+      {
+        _id: "001",
+        listData: ["Our Story", "Our Team", "Careers", "Press Releases"],
+      },
+    ],
+  },
+  {
+    _id: 2222,
+    title: "Make Money with Us",
+    listItem: [
+      {
+        _id: "002",
+        listData: [
+          "Affiliate Program",
+          "Sell on Our Platform",
+          "Advertise with Us",
+          "Become a Partner",
+        ],
+      },
+    ],
+  },
+  {
+    _id: 2223,
+    title: "Shoppers Payment Products",
+    listItem: [
+      {
+        _id: "003",
+        listData: [
+          "Credit Cards",
+          "Gift Cards",
+          "Wallet Services",
+          "Buy Now, Pay Later",
+        ],
+      },
+    ],
+  },
+  {
+    _id: 2224,
+    title: "Let Us Help You",
+    listItem: [
+      {
+        _id: "004",
+        listData: [
+          "Customer Support",
+          "Returns and Exchanges",
+          "FAQs",
+          "Shipping and Delivery",
+        ],
+      },
+    ],
+  },
+];
